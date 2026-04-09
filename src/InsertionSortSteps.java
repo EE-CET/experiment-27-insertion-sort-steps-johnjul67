a201f2513cd52451683b263dfc2f9022818377fa
John <tve24el033@cet.ac.in>
2026-04-09 18:37:23 +0530

    c2

--- src/InsertionSortSteps.java
+++ src/InsertionSortSteps.java
@@ -1,29 +1,39 @@
 import java.util.Scanner;
-public class InsertionSortSteps 
-{
-        static void print(int[] arr)
-        {
-                for (int i=0;i<arr.length;i++)
-                        System.out.print(arr[i] + " ");
-                System.out.println();
-        }
-        public static void main(String[] args)
-        {
-                Scanner sc = new Scanner(System.in);
-                int n = sc.nextInt();
-                int arr[] = new int[n];
-                for (int i=0;i<n;i++)
+
+public class InsertionSortSteps{
+        public static void main(String args[]){
+                Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();
+                int arr[]=new int[n];
+                for(int i=0;i<n;i++){
                         arr[i]=sc.nextInt();
-                int key = arr[n - 1];
-                int i = n - 2;
-                while (i>=0&&arr[i]>key)
-                {
+                }
+                int key=arr[n-1];
+                int i=n-2;
+                while(i>=0 && arr[i]>key){
                         arr[i+1]=arr[i];
-                        print(arr);
+                        print(n,arr);
                         i--;
                 }
                 arr[i+1]=key;
-                print(arr);
+                print(n,arr);
+        }
+        public static void print(int n, int arr[]) {
+                for (int i = 0; i < n; i++) {
+                System.out.print(arr[i]);
+                if (i < n - 1) System.out.print(" ");
+                }
+                System.out.println();
         }
 
+        // TODO: Read n
+        // TODO: Read the array elements
+
+        // TODO: Implement the insertion logic:
+        // 1. Store the last element (key)
+        // 2. Iterate backwards from the second-to-last element
+        // 3. If current element > key, shift it right and print array
+        // 4. If current element <= key, insert key and print array (then break)
+        // Note: Don't forget the case where key is the smallest element (index 0)
+    
 }
